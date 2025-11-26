6a15ea6a51990120a537befe6f4a561aa6dd09f4
Sgambax <ibrahimmichlane646@gmail.com>
2025-11-26 12:47:39 +0000

    es1.c

--- es1.c
+++ es1.c
@@ -3,6 +3,23 @@
 #include <stdio.h>
 
 int main(){
+  printf("Inserisci 2 numeri");
 
-    return 0;
+    int numero_1, numero_2;
+
+    scanf("%d", &numero_1);
+        scanf("%d", &numero_2);
+if(numero_1 > numero_2) {
+
+    printf("il primo numero: %d è maggiore del secondo: %d\n", numero_1, numero_2);
+
+  
+} else { 
+  
+  printf("Il primo numero: %d è minore del secondo: %d\n", numero_1, numero_2);
+
+}
+
+return 0;
+
 }
